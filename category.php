63e38abd79e6dd387f13a4a45081fa43a04b90d3
developer <[omitted]>
2019-04-14 02:17:50 +0545

    Update category

--- category.php
+++ category.php
@@ -1,178 +1,150 @@
 <?php get_header(); ?>
-    <div class="catagory-items">
+    <div class="latest-news-block">
         <div class="container">
             <div class="row">
                 <div class="col-sm-9">
-                    <div class="top-texonomy">
+                    <div class="top-news-block">
                         <?php
                         $term = get_queried_object();
                         wp_reset_query();
-                        $args = array('post_type' => 'news',
-                            'orderby' => 'date',
-                            'order' => 'DESC',
+                        $args = array(
+                            'post_type'      => 'news',
+                            'orderby'        => 'date',
+                            'order'          => 'DESC',
                             'posts_per_page' => 1,
-                            'tax_query' => array(
+                            'tax_query'      => array(
                                 array(
                                     'taxonomy' => 'news_category',
-                                    'field' => 'slug',
-                                    'terms' => $term->slug,
+                                    'field'    => 'slug',
+                                    'terms'    => $term->slug,
                                 ),
                             ),
                         );
 
                         $loop = new WP_Query($args);
-                        if($loop->have_posts()) {
-                            while($loop->have_posts()) : $loop->the_post();
-                                echo '<div class="texonomy-post">
-                                        
-                                        <div class="texonomy-post-content">
-                                            <div>
-                                                <div class="texonomy-content-box">
-                                                    <h2 class="texonomy-post-title">
-                                                        <a href="'.get_permalink().'">'.get_the_title().'</a>
-                                                <p><span>'.get_the_date().'</span></p>
-
-                                                    </h2>
-                                                    
-                                                </div>
-                                                <div class="texonomy-img">
-                                                    <a href="'.get_permalink().'">'.get_the_post_thumbnail().'</a>
-                                                    <p class="texonomy-content">
-                                                        '.substr( get_the_content(), 0, 350 ).'
-                                                    </p>
-                                                </div>
-                                                
-                                            </div>
-                                        </div>
-                                    </div>';
-                            endwhile;
-                        }
-                        ?>
-                    </div>
-                    <div class="row">
-                        <?php
-                        $term = get_queried_object();
-                        wp_reset_query();
-                        $args = array('post_type' => 'news',
-                            'offset' => 1,
-                            'orderby' => 'date',
-                            'order' => 'DESC',
-                            'tax_query' => array(
-                                array(
-                                    'taxonomy' => 'news_category',
-                                    'field' => 'slug',
-                                    'terms' => $term->slug,
-                                ),
-                            ),
-                        );
-
-                        $loop = new WP_Query($args);
-                        if($loop->have_posts()) {
-                            while($loop->have_posts()) : $loop->the_post();
-                                echo'<div class="col-sm-6">
-                                            <div class="texonomy-post-content">
-                                                <a href="'.get_permalink().'">'.get_the_post_thumbnail().'</a>
-                                                <h2 class="texonomy-post-title">
-                                                    <a href="'.get_permalink().'">'.get_the_title().'</a>
-                                                </h2>
-                                                <span>'.get_the_date().'</span>
-                                                <p class="texonomy-content">
-                                                    '.substr( get_the_content(), 0, 350 ).'
-                                                </p>
-                                            </div>
-                                        </div>';
-                            endwhile;
+                        if ($loop->have_posts()) {
+                            while ($loop->have_posts()) : $loop->the_post(); ?>
+                                <div class="top-news-img">
+                                    <?php the_post_thumbnail("medium_large"); ?>
+                                    <div class="overlay"></div>
+                                </div>
+                                <div class="top-news-content">
+                                    <div class="latest-news-title">
+                                        <a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a>
+                                    </div>
+                                    <div class="latest-news-content">
+                                        <?php $content = get_the_content();
+                                        echo mb_strimwidth($content, 0, 125, '...') ?>
+                                    </div>
+                                </div>
+                            <?php endwhile;
                         }
                         ?>
                     </div>
                 </div>
                 <div class="col-sm-3">
-                    <div class=" sidebar top-stories">
-                        <div class="sidebar-title">Popular Stories</div>
-                        <ul class="news-list">
-                            <?php
-                            $args = array(
-                                'post_type' => 'news',
-                                'post_status' => 'publish',
-                                'posts_per_page' =>4,
-                                'order_by'=>'date',
-                                'order'=>'DESC',
-                                'meta_key' => 'wpb_post_views_count',
-                                'orderby' => 'meta_value_num'
-                            );
-                            $loop = new WP_Query( $args );
-                            while ( $loop->have_posts() ) : $loop->the_post();
-                                ?>
-                                <li>
-                                    <div class="catt">
-                                        <?php
-                                        $terms = get_the_terms(get_the_ID(),'news_category' );
-                                        if ( $terms && ! is_wp_error( $terms ) ) :
-
-                                            $catt_links = array();
-
-                                            foreach ( $terms as $term ) {
-                                                $catt_links[] = $term->name;
-                                            }
-
-                                            $news_category = join( ", ", $catt_links );
-                                            ?>
-
-                                            <p class="news-category">
-                                                <?php printf( esc_html__( '%s', 'textdomain' ), esc_html( $news_category ) ); ?>
-                                            </p>
-                                        <?php endif; ?>
-                                    </div>
-                                    <h3 class="latest-news-titles">
-                                        <a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a>
-                                    </h3>
-                                    <div class="posted-date">
-                                        <i class="sm-icon-date">
-                                        </i><?php the_time('F jS, Y'); ?>
-                                    </div>
-                                </li>
-                            <?php
-                            endwhile;
-                            wp_reset_postdata();
-                            ?>
-                        </ul>
-                    </div>
                     <div class="sidebar">
                         <div class="news-latest-list latest-list">
-                            <div class="sidebar-title">Category</div>
-                            <?php
-                            $terms = get_terms(
-                                array(
-                                    'taxonomy'   => 'news_category',
-                                    'hide_empty' => false,
-                                )
-                            );
-
-                            // Check if any term exists
-                            if ( ! empty( $terms ) && is_array( $terms ) ) {
-                                // Run a loop and print them all
-                                foreach ( $terms as $term ) { ?>
-                                    <ul>
+                            <div class="sidebar-title">Popular News</div>
+                            <ul class="cat-news-popular">
+                                <?php
+                                    global $post;
+                                    $args    = array(
+                                        'numberposts' => 4,
+                                        'post_type'   => 'news',
+                                        'meta_key'    => 'wpb_post_views_count',
+                                        'orderby'     => 'meta_value_num',
+                                        'order'       => 'DESC'
+                                    );
+                                    $myposts = get_posts($args);
+                                    foreach ($myposts as $post) : setup_postdata($post); ?>
                                         <li>
-                                            <a href="<?php echo esc_url( get_term_link( $term ) ) ?>">
-                                                <?php echo $term->name; ?>
-                                            </a>
+                                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                                         </li>
-                                    </ul>
-                                    <?php
-                                }
-                            }
-                            ?>
-                        </div>
-                        <div class="sidebar-block">
-                            <div class="side-subscribe-section">
-                                <?php echo do_shortcode('[subscribe_email layout="normal"]') ?>
-                            </div>
+                                        <?php wpb_track_post_views(get_the_ID()); ?>
+                                    <?php endforeach;
+                                ?>
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+<?php
+$term = get_queried_object();
+
+wp_reset_query();
+$args  = array(
+    'post_type'      => 'news',
+    'posts_per_page' => 3,
+    'offset'         => 1,
+    'orderby'        => 'date',
+    'order'          => 'DESC',
+    'tax_query'      => array(
+        array(
+            'taxonomy' => 'news_category',
+            'field'    => 'slug',
+            'terms'    => $term->slug,
+        ),
+    ),
+);
+$query = new WP_Query($args);
+
+$news_posts_array = [];
+while ($query->have_posts()) {
+    $query->the_post();
+
+    $title          = get_the_title();
+    $content        = get_the_content();
+    $featured_image = get_the_post_thumbnail_url();
+    $categories     = get_the_terms(get_the_ID(), 'news_category');
+    $published_date = get_the_date();
+    $permalink      = get_the_permalink();
+
+    $image_id = get_post_thumbnail_id();
+    list($url, $width, $height) = wp_get_attachment_image_src($image_id, 'post-thumbnail');
+
+    $post_category = [];
+    foreach ($categories as $category) {
+        if (sizeof($categories) > 1) {
+            array_push($post_category, $category->name);
+        } else {
+            array_push($post_category, $category->name);
+        }
+    }
+
+
+    $news_post_single = array(
+        'title'          => $title,
+        'content'        => $content,
+        'featured_image' => $featured_image,
+        'category'       => $post_category,
+        'width'          => $width,
+        'height'         => $height,
+        'published_date' => $published_date,
+        'permalink'      => $permalink
+    );
+    array_push($news_posts_array, $news_post_single);
+}
+$newsJSON = json_encode($news_posts_array);
+?>
+    <div class="news-listing-cat">
+        <div class="container">
+            <div class="masonry"></div>
+        </div>
+        <div class='loader'>
+            <div class='spinner'>
+                <div class='dot1'></div>
+                <div class='dot2'></div>
+            </div>  
+        </div>
+        <input type="hidden" class="showMore" data-page="1" data-url="<?php echo admin_url('admin-ajax.php'); ?>" data-category="<?php echo $term->slug; ?>">
+    </div>
+
+    <script>
+        var json =  <?php echo $newsJSON;?>;
+    </script>
+    
 
 <?php get_footer(); ?>
